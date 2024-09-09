506cd7562b4e3063d9027ddbb9f56d0bace6763a
Kalachik Roman <romankalachik@eremex.ru>
2024-09-09 10:06:35 +0300

    first commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    IList<EmployeeInfo> employees;
+
+    public MainWindowViewModel()
+    {
+        Employees = new List<EmployeeInfo>();
+    }
+
+}
+
+public class EmployeeInfo
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public DateTime HiredAt { get; set; }
+    public int Experience { get; set; }
+    public string Position { get; set; }
+    public bool Married { get; set; }
+    public string City { get; set; }
+    public string Phone { get; set; }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+using Eremex.AvaloniaUI.Controls.Common;
+
+namespace AvaloniaApplication1.Views;
+
+public partial class MainWindow : MxWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
